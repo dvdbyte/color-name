05fad959ee0b3f073f9131f860b7d46462705c9b
dvdbyte <dvdbyte1@gmail.com>
2024-02-01 13:19:35 +0000

    version-1

---
+++ js/main.js
@@ -0,0 +1,162 @@
+document.getElementById('blue').addEventListener('click', blue)
+document.getElementById('yellow').addEventListener('click', yellow)
+document.getElementById('red').addEventListener('click', red)
+document.getElementById('green').addEventListener('click', green)
+document.getElementById('orange').addEventListener('click', orange)
+document.getElementById('purple').addEventListener('click', purple)
+document.getElementById('teal').addEventListener('click', teal)
+document.getElementById('chartreuse').addEventListener('click', chartreuse)
+document.getElementById('vermilion').addEventListener('click', vermilion)
+document.getElementById('magenta').addEventListener('click', magenta)
+document.getElementById('violet').addEventListener('click', violet)
+document.getElementById('amber').addEventListener('click', amber)
+
+function blue(){
+  document.querySelector('body').style.backgroundColor = 'blue'
+  document.querySelector('#blue').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'blue'
+  document.querySelector('body').style.color = 'white'
+}
+
+function yellow() {
+  document.querySelector('body').style.backgroundColor = 'yellow'
+  document.querySelector('#yellow').style.border = '2px solid black'
+  document.querySelector('#name').innerHTML = 'Yellow'
+}
+
+function red() {
+  document.querySelector('body').style.backgroundColor = 'red'
+  document.querySelector('#red').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Red'
+  document.querySelector('body').style.color = 'white'
+}
+function green(){
+  document.querySelector('body').style.backgroundColor = 'green'
+  document.querySelector('#green').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Green'
+  document.querySelector('body').style.color = 'white'
+}
+
+function orange() {
+  document.querySelector('body').style.backgroundColor = 'orange'
+  document.querySelector('#orange').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Orange'
+  document.querySelector('body').style.color = 'white'
+}
+
+function purple() {
+  document.querySelector('body').style.backgroundColor = 'purple'
+  document.querySelector('#purple').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Purple'
+}
+function teal(){
+  document.querySelector('body').style.backgroundColor = 'teal'
+  document.querySelector('#teal').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Teal'
+  document.querySelector('body').style.color = 'white'
+}
+
+function chartreuse() {
+  document.querySelector('body').style.backgroundColor = 'chartreuse'
+  document.querySelector('#chartreuse').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Chartreuse'
+  document.querySelector('body').style.color = 'white'
+}
+
+function vermilion() {
+  document.querySelector('body').style.backgroundColor = 'rgb(227, 66, 52)'
+  document.querySelector('#vermilion').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Vermilion'
+  document.querySelector('body').style.color = 'white'
+}
+function magenta(){
+  document.querySelector('body').style.backgroundColor = 'magenta'
+  document.querySelector('#magenta').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Magenta'
+  document.querySelector('body').style.color = 'white'
+}
+
+function violet() {
+  document.querySelector('body').style.backgroundColor = 'violet'
+  document.querySelector('#violet').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Violet'
+  document.querySelector('body').style.color = 'white'
+}
+
+function amber() {
+  document.querySelector('body').style.backgroundColor = 'rgb(255,191,0)'
+  document.querySelector('#amber').style.border = '2px solid white'
+  document.querySelector('#name').innerHTML = 'Amber'
+  document.querySelector('body').style.color = 'white'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
